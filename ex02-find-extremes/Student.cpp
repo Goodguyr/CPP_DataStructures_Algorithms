d308f796cdb353475d3f9e25d39ece0a694d5f1e
Goodguyr <50654159+Goodguyr@users.noreply.github.com>
2020-09-08 11:01:49 +0300

    Student compareTo function added

---
+++ ex02-find-extremes/Student.cpp
@@ -0,0 +1,22 @@
+#include "Student.h"
+
+using namespace ds_course;
+using namespace std;
+
+Student::Student(){}
+
+int Student::compareTo(Student ss){
+    if(this->age > ss.age){
+        return 1;
+    }
+    else if(this->age == ss.age){
+        if(this->height > ss.height){
+            return 1;
+        }
+        else if(this->height < ss.height){
+            return -1;
+        }
+        return 0;
+    }
+    return -1;
+}
